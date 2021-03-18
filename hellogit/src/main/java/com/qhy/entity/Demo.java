95ed3a85ddab6149403dc06f9c09c5843e5558ec
邱焕宜 <qiuhuanyi@126.com>
2021-03-18 14:55:42 +0800

    branch dev

--- hellogit/src/main/java/com/qhy/entity/Demo.java
+++ hellogit/src/main/java/com/qhy/entity/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("更新1");
+        System.out.println("创建分支");
 
     }
 }
